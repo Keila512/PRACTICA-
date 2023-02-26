695d9e96b55866e9df0b8729f9e81c664a452968
keila <soliskeila383@gmail.com>
2023-02-25 20:43:49 -0600

    Add project files.

---
+++ PRACTICA STREAMRYW/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PRACTICA_STREAMRYW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnEscribr = new System.Windows.Forms.Button();
+            this.richTxtBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(200, 67);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(122, 47);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "LEER";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnEscribr
+            // 
+            this.btnEscribr.Location = new System.Drawing.Point(471, 67);
+            this.btnEscribr.Name = "btnEscribr";
+            this.btnEscribr.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnEscribr.Size = new System.Drawing.Size(116, 47);
+            this.btnEscribr.TabIndex = 1;
+            this.btnEscribr.Text = "ESCRIBIR";
+            this.btnEscribr.UseVisualStyleBackColor = true;
+            this.btnEscribr.Click += new System.EventHandler(this.btnEscribr_Click);
+            // 
+            // richTxtBox
+            // 
+            this.richTxtBox.Location = new System.Drawing.Point(37, 139);
+            this.richTxtBox.Name = "richTxtBox";
+            this.richTxtBox.Size = new System.Drawing.Size(707, 299);
+            this.richTxtBox.TabIndex = 2;
+            this.richTxtBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTxtBox);
+            this.Controls.Add(this.btnEscribr);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnEscribr;
+        private System.Windows.Forms.RichTextBox richTxtBox;
+    }
+}
+
---
+++ PRACTICA STREAMRYW/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRACTICA_STREAMRYW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt|*.txt";
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            
+            StreamReader sr = new StreamReader(ofd.FileName);
+            richTxtBox.Text = sr.ReadToEnd();
+            sr.Close();
+
+        }
+
+        private void btnEscribr_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt|*.txt";
+            if (sfd.ShowDialog() != DialogResult.OK) 
+            {
+                return; 
+            }
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+            sw.Write(richTxtBox.Text);
+            sw.Close();
+
+        }
+    }
+}
